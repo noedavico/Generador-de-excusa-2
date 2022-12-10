f348b1f094703e1ab9f2a1ca9c5c566142a8f06c
noedavico <noedavico@hotmail.com>
2022-12-10 21:40:52 +0000

    entrega

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Arrays;
+let who = [
+  "El perro",
+  "El gato",
+  "Mi hamster",
+  "La tortuga",
+  "Mi abuela",
+  "Mi madre",
+  "Mi vecino",
+  "Mi hijo",
+  "Mi hermano"
+];
+let action = ["comió", "destruyo", "meó", "aplastó", "rompió", "escondio"];
+let what = [
+  "mi tarea",
+  "las llaves",
+  "el auto",
+  "el celular",
+  "el control",
+  "la planta",
+  "el libro"
+];
+let when = [
+  "antes de la clase",
+  "justo a tiempo",
+  "cuando jugaba",
+  "durante el almuerzo",
+  "mientras estaba orando",
+  "cuando meditaba",
+  "mientras me duchaba",
+  "cuando dormia",
+  "mientras estudiaba",
+  "cuando estaba trabajando"
+];
+//random
+let randWho = Math.floor(Math.random() * who.length);
+let randAction = Math.floor(Math.random() * action.length);
+let randWhat = Math.floor(Math.random() * what.length);
+let randWhen = Math.floor(Math.random() * when.length);
+
+//Selector
+var excuse = document.querySelector("#excuse");
+
+function TheExcuse() {
+  excuse.innerHTML = `${who[randWho]}
+  ${action[randAction]}
+    ${what[randWhat]}
+      ${when[randWhen]}`;
+}
+/*Onload*/
+window.onLoad = TheExcuse();
